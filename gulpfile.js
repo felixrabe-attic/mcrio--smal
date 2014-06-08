7d0a4f158c4c79959eef1535ee7a37acda8fba17
Felix Rabe <felix@rabe.io>
2014-06-08 14:42:41 +0200

    There is no gutil.error()

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 
 gulp.task('test', function() {
-  gutil.error('Version 0.0.0 - actual code coming soon.');
+  gutil.log('Version 0.0.0 - actual code coming soon.');
 });
