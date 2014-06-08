77c28a1bbfe49d435ccc9a60be7c36a01142b628
Felix Rabe <felix@rabe.io>
2014-06-08 14:56:04 +0200

    Rewrite gulpfile.js in CoffeeScript

---
+++ gulpfile.coffee
@@ -0,0 +1,5 @@
+gulp = require 'gulp'
+gutil = require 'gulp-util'
+
+gulp.task 'test', ->
+  gutil.log 'Version 0.0.0 - actual code coming soon.'
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,2 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-
-gulp.task('test', function() {
-  gutil.log('Version 0.0.0 - actual code coming soon.');
-});
+require('coffee-script/register');
+require('./gulpfile.coffee');
